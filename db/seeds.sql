e845f37968c7d2cd383e7f9dd53e1da0bcb5be12
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-02-12 16:07:23 +1030

    added readme file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 insert into `burgers_db`.`burgers` (`burger_name`) values ('Big Mac');
 insert into burgers (burger_name) values ('Wooper');
 insert into burgers (burger_name) values ('Zinger');
+insert into burgers (burger_name, devoured) values ('Fish fillet', true);
