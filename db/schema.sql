8643950152ccfd699b2e3bdeb0424e4579331b08
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-02-12 21:42:23 +1030

    updated seed.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,8 @@ use burgers_db;
 create table burgers (
 id int auto_increment primary key not null,
 burger_name varchar(30) not null,
-devoured bool not null default false
+devoured bool not null default false,
+createdAt TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP
 );
-
 
 
